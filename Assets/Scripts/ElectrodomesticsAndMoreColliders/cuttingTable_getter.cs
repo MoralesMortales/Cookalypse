6ddcbe5a52e431d5c7dd0b3d4dcea3303269dfdb
Carlos Morales <carlos52.7624@gmail.com>
2025-05-03 19:19:15 -0700

    finally its done

--- Assets/Scripts/ElectrodomesticsAndMoreColliders/cuttingTable_getter.cs
+++ Assets/Scripts/ElectrodomesticsAndMoreColliders/cuttingTable_getter.cs
@@ -3,20 +3,12 @@ using UnityEngine.InputSystem;
 
 public class TagDetector : MonoBehaviour
 {
-    [SerializeField]
-    private string tagToDetect = "canCut";
-
-    [SerializeField]
-    private GameObject tomatoToActivate;
-
-    [SerializeField]
-    private GameObject onionToActivate;
-
+    [SerializeField] private string tagToDetect = "canCut";
+    [SerializeField] private GameObject tomatoToActivate;
+    [SerializeField] private GameObject onionToActivate;
+    [SerializeField] private GameObject tomatoSlicePrefab;
+    
     public PickUpScript toolUsing;
-
-    [SerializeField]
-    private GameObject tomatoSlicePrefab;
-
     private bool knifeUsing = false;
     private bool withFood = false;
 
@@ -25,11 +17,8 @@ public class TagDetector : MonoBehaviour
         if (toolUsing == null)
         {
             toolUsing = FindObjectOfType<PickUpScript>();
-
             if (toolUsing == null)
-            {
                 Debug.LogError("No se encontró PickUpScript en la escena.");
-            }
         }
     }
 
@@ -37,69 +26,50 @@ public class TagDetector : MonoBehaviour
     {
         if (toolUsing.currentToolData != null)
         {
-            Debug.Log($"Usando herramienta: {toolUsing.currentToolData}");
-            if (toolUsing.currentToolData == "Knife")
-            {
-                knifeUsing = true;
-            }
-            else
-            {
-                knifeUsing = false;
-            }
+            knifeUsing = (toolUsing.currentToolData == "Knife");
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         AssignMultipleTags otherTags = other.GetComponent<AssignMultipleTags>();
-
-        if (otherTags == null)
-            return;
+        if (otherTags == null) return;
 
         if (otherTags.HasTag("Player"))
         {
-            Debug.Log("hola player");
-
             CurrentTool();
         }
 
-        if (otherTags != null && otherTags.HasTag(tagToDetect))
+        if (otherTags.HasTag(tagToDetect))
         {
-            if (otherTags.HasTag("tomato"))
+            if (otherTags.HasTag("tomato") && tomatoToActivate != null)
             {
-                if (tomatoToActivate != null)
-                {
-                    Debug.Log("Tomato para picar");
-                    tomatoToActivate.SetActive(true);
-                    withFood = true;
-                }
+                tomatoToActivate.SetActive(true);
+                withFood = true;
                 other.gameObject.SetActive(false);
             }
-            else if (otherTags.HasTag("onion"))
+            else if (otherTags.HasTag("onion") && onionToActivate != null)
             {
-                if (onionToActivate != null)
-                {
-                    onionToActivate.SetActive(true);
-                    withFood = true;
-                }
+                onionToActivate.SetActive(true);
+                withFood = true;
                 other.gameObject.SetActive(false);
             }
         }
+    }
 
-        if (knifeUsing && withFood && (Input.GetKeyDown(KeyCode.F)))
+    private void Update()
+    {
+        if (knifeUsing && withFood && Input.GetKeyDown(KeyCode.F))
         {
             Debug.Log("picado, nan nan nan");
-        }
-        else
-        {
-            Debug.Log(
-                "tool "
-                    + knifeUsing
-                    + " with food"
-                    + withFood
-                    + "Key "
-                    + (Input.GetKeyDown(KeyCode.F))
-            );
+            // Aquí deberías instanciar el tomatoSlicePrefab
+            if (tomatoSlicePrefab != null)
+            {
+                Instantiate(tomatoSlicePrefab, transform.position, transform.rotation);
+            }
+            withFood = false;
+            if (tomatoToActivate != null) tomatoToActivate.SetActive(false);
+            if (onionToActivate != null) onionToActivate.SetActive(false);
         }
     }
 }

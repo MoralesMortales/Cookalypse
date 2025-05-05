24953bc6fcb4175ee162a078fe66255730468289
Carlos Morales <carlos52.7624@gmail.com>
2025-05-04 18:58:49 -0700

    almost there

--- Assets/Scripts/ElectrodomesticsAndMoreColliders/stove.cs
+++ Assets/Scripts/ElectrodomesticsAndMoreColliders/stove.cs
@@ -22,7 +22,7 @@ public class stove : MonoBehaviour
 
     void Start()
     {
-        spawnPoint = new Vector3(-16.171f, 1.059f, 22.966f);
+        spawnPoint = new Vector3(0.28f, 0.01f, 0.41f);
         if (toolUsing == null)
         {
             toolUsing = FindObjectOfType<PickUpScript>();
